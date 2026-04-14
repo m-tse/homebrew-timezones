e65364be5a1845e1e63ca192e1c46166d4eeafa2
Matthew Tse <matthew@matthewtse.com>
2026-04-14 16:38:32 +0800

    Update to v1.5.0

--- Casks/timezones.rb
+++ Casks/timezones.rb
@@ -1,6 +1,6 @@
 cask "timezones" do
-  version "1.4.0"
-  sha256 "22e749a419c1f7773a9e416bdb4ca56f91da2c6449ea84f25c2b9b7c4bda6545"
+  version "1.5.0"
+  sha256 "83c407ff2a3f5f1fde7297caefa26aa6242674faae5c22df2beb16e1ee4d9d79"
 
   url "https://github.com/m-tse/TimeZonesMenuBarApp/releases/download/v#{version}/TimeZones-#{version}.zip"
   name "Time Zones"
